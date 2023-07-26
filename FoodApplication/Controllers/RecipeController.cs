5fc7a8856b1f376cb8052a4165f3fd87c047164e
duc-tai0404 <114871782+duc-tai0404@users.noreply.github.com>
2023-07-26 11:28:15 +0700

    Add project files.

---
+++ FoodApplication/Controllers/RecipeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FoodApplication.Controllers
+{
+    public class RecipeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ FoodApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FoodApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
